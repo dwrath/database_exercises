baa8ed08f8bf9856a9c525cd6b3fe7f010cff990
Damian Wrather <damian.wrather@gmail.com>
2023-05-01 15:05:36 -0500

    codeup_test_db

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist VARCHAR(255) NOT NULL,
+                        name VARCHAR(255) NOT NULL,
+                        release_date INT NOT NULL,
+                        sales FLOAT NOT NULL,
+                        genre VARCHAR(255) NOT NULL,
+                        PRIMARY KEY (id)
+);
