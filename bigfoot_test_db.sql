3e09e106ff9b05cbe82fa4d3ad47c60e8b759434
Damian Wrather <damian.wrather@gmail.com>
2023-05-01 12:21:08 -0500

    Edit

--- bigfoot_test_db.sql
+++ bigfoot_test_db.sql
@@ -1,11 +1,11 @@
 USE bigfoot_test_db;
 
-Drop table IF exists users;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(35) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (user_id),
+    PRIMARY KEY (user_id)
 );
